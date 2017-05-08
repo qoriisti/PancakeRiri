8e3e61148e459f973001e53b87823bb728d421bc
syifaDwiR <syifa425@gmail.com>
2017-05-08 12:07:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/toshiba/pancakeriri/TopMenuActivity.java
@@ -0,0 +1,26 @@
+package com.example.toshiba.pancakeriri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class TopMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_menu);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){ //Bila menekan tombol back maka akan kembali ke MainActivity.xml
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
+
+
